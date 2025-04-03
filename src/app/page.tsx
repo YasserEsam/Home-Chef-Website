b014aa36d60dcf3fc8dd22b2e5a50ab3c423f99a
YasserEsam <yaaser10esam1010@gmail.com>
2025-04-03 18:12:17 +0300

    feat : add the navbar with responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <main className='bg-white py-6 px-12'>
+      <div className='container mx-auto'>
+
+      </div>
+    </main>
   )
 }
 
